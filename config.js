83917f64b52a58db80c3d6da926478ce1b3eb2b7
Sandy-Burnley <51831763+Sandy-Burnley@users.noreply.github.com>
2020-03-13 11:57:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -91,7 +91,7 @@ var config = {
     "id": "puck",
     "name": "Puck",
     "type": "geoJSON",
-    "cluster": false,
+    "cluster": true,
     "showCoverageOnHover": true,
     "minZoom": 12,
     "url": "./data/puck.geojson",
