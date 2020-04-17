420a092e122c6f4a56c165217e32d58d596dd971
Sandy-Burnley <51831763+Sandy-Burnley@users.noreply.github.com>
2020-04-17 10:26:57 -0400

    added hounds

--- custom.js
+++ custom.js
@@ -7,29 +7,29 @@ function beforeMapLoads(){
 
 	// // Create a layer which is based on a query string in the URL - this shows the US state based on the query
 	// // value, eg bootleaf.html/?query=california
-// var houndsConfig = {
-	// 	"id": "Hounds",
-	// 	"name": "Hounds",
-	// 	"type": "agsFeatureLayer",
-	// 	"url": "https://services.arcgis.com/uHAHKfH1Z5ye1Oe0/arcgis/rest/services/Hounds/FeatureServer",
-	// 	"layers": [19],
-	// 	"useCors": false,
-	// 	"visible": true
-	// }
+var houndsConfig = {
+		"id": "Hounds",
+		"name": "Hounds",
+		"type": "agsFeatureLayer",
+	 	"url": "https://services.arcgis.com/uHAHKfH1Z5ye1Oe0/arcgis/rest/services/Hounds/FeatureServer",
+	 	"layers": [19],
+		"useCors": false,
+		"visible": true
+	 }
 
-	// var query = getURLParameter('query');
-	// if(query) {
-	// 	houndsConfig.layerDefs = "19: Title = '" + query + "'";
-	// 	houndsConfig.name += " (" + query + ")";
-	// }
+	 var query = getURLParameter('query');
+	 if(query) {
+	/	houndsConfig.layerDefs = "19: Title = '" + query + "'";
+	 	houndsConfig.name += " (" + query + ")";
+	 }
 
 	// // Add this layer to the TOC and map.
-	 // config.layers.push(houndsConfig);
-	// for (i in config.tocCategories){
-	// 	if (config.tocCategories[i]['name'] === 'ArcGIS Layers') {
-	// 		config.tocCategories[i]['layers'].push(houndsConfig.id);
-	// 	}
-	// }
+	  config.layers.push(houndsConfig);
+	 for (i in config.tocCategories){
+	 	if (config.tocCategories[i]['name'] === 'ArcGIS Layers') {
+	 		config.tocCategories[i]['layers'].push(houndsConfig.id);
+	 	}
+	 }
 	
 	// // If there are any layers defined in the URL, add this layer to the list so it draws by default
 	// if(bootleaf.layerParams.length > 0){
@@ -39,10 +39,6 @@ function beforeMapLoads(){
 }
 
 function afterMapLoads(){
-	// This function is run after the map has loaded. It gives access to bootleaf.map, bootleaf.TOCcontrol, etc
-	
-	// Check to see whether the Gray basemap is chosen, and the user has zoomed in too far. In this case, 
-	// switch to the Streets basemap
 	bootleaf.map.on("zoomend", function(evt){
 		if (bootleaf.currentBasemap === 'Gray'){
 			if (evt.target._zoom >= 17) {
